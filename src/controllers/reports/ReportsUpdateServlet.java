7accdd41f8fac29aaf2c1894af6e26da0ddef52e
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2019-08-26 20:06:52 +0900

    Add Report new

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -1,6 +1,7 @@
 package controllers.reports;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +13,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/reports/update")
 public class ReportsUpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +23,11 @@ public class ReportsUpdateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
 
 }
